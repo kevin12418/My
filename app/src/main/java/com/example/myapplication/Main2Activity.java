93294cc27b69a34ca372447ec7402c78cb50104c
bin0002 <pete0814@naver.com>
2019-11-09 17:41:16 +0900

    P2

---
+++ app/src/main/java/com/example/myapplication/Main2Activity.java
@@ -0,0 +1,20 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.ViewAnimationUtils;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        overridePendingTransition(R.anim.ani,0);
+        Animation animation = AnimationUtils.loadAnimation(this,R.anim.ani2);
+        findViewById(R.id.a).startAnimation(animation);
+    }
+}
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void click(View v){
+        startActivity(new Intent(this,Main2Activity.class));
     }
 };
